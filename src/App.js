d5ab4977b5a10d76a996525ba3cc7474456b0141
yasuyokuni <yasuyokuni@gmail.com>
2024-04-23 02:20:14 +0800

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
 import * as tf from '@tensorflow/tfjs';
+import * as dsp from 'dsp.js-browser';
 
 function App() {
   const [selectedFile, setSelectedFile] = useState();
   const [model, setModel] = useState(null);
+  const [audioBuffer, setAudioBuffer] = useState();
 
   const handleFileUpload = event => {
     setSelectedFile(event.target.files[0]);
@@ -14,9 +16,35 @@ function App() {
     if (!selectedFile) {
       return;
     }
-    // Upload the file to a server here
-    // This is just a placeholder and won't actually upload the file
-    console.log(selectedFile);
+    
+    const audioContext = new AudioContext();
+    const source = audioContext.createBufferSource();
+    const analyser = audioContext.createAnalyser();
+    const arrayBuffer = await selectedFile.arrayBuffer();
+    audioContext.decodeAudioData(arrayBuffer, (audioBuffer) => {
+      setAudioBuffer(audioBuffer)
+      source.buffer = audioBuffer;
+      
+      analyser.fftSize = 512;
+
+
+      source.connect(analyser);
+      source.start();
+
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+      analyser.getByteFrequencyData(dataArray);
+
+      console.log(Array.from(dataArray))
+
+      const float = audioBuffer.getChannelData(0);
+      console.log(float)
+      // const fft = new dsp.FFT(2048, 44100)
+      // var spectrum = fft.spectrum;
+      // console.log(spectrum, 'jjijdisjdisjdisj')
+    
+    })
+    
   };
 
   const loadModel = async () => {
@@ -26,6 +54,7 @@ function App() {
       const input = tf.randomNormal([1, 57])
       // const input = tf.zeros([[192,192]], 'int32')
       console.log(input, 'mmmmm')
+      //['country', 'pop', 'hiphop', 'rock', 'metal', 'reggae', 'disco', 'classical', 'jazz', 'blues']
       const output = model.predict(input)
       const array = await output.array()
       console.log(array, 'aaarerr')
@@ -36,16 +65,16 @@ function App() {
   }
 
   useEffect(() => {
-    // loadModel();
-   
-
+    loadModel();
   }, []);
 
-
+useEffect(() => {
+  console.log(audioBuffer, 'djiijij')
+}, [audioBuffer])
   return (
     <div className="App">
       <input
-        accept="image/*"
+        accept="audio/*"
         style={{ display: 'none' }}
         id="raised-button-file"
         type="file"
